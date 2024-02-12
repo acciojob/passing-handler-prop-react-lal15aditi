4a08c90a791d58629816b61d27a4517d49b58eb2
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-02-13 00:37:35 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+// Selection.js
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor, 'data-testid': dataTestId }) => {
+  const [selectionStyle, updateSelectionStyle] = useState({});  
+
+  console.log(selectionStyle);
+
+  return (
+    <div className='fix-box' style={selectionStyle} onClick={() => applyColor(updateSelectionStyle)} data-testid={dataTestId}>
+      <h1>Selection</h1>
+    </div>
+  );
+}
+
+export default Selection;
